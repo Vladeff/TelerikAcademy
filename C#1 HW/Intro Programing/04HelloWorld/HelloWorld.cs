0fa5be36dcd62d1adbcf9455b6bc35f2fec111fa
Vladeff <vladivivaldi@abv.bg>
2015-01-17 20:20:11 +0200

    how to

---
+++ C#1 HW/Intro Programing/04HelloWorld/HelloWorld.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class helloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello C#!");
+    }
+}
---
+++ C#1 HW/Intro Programing/SquareRoot/squareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class squareRoot
+{
+    static void Main()
+    {
+        int numberToSquare = 12345;
+        Console.WriteLine (Math.Sqrt (numberToSquare));
+    }
+}
