cf1ca68828669b7572930dd8d8e7b6a60e5f81ec
Vladeff <vladivivaldi@abv.bg>
2015-01-17 20:14:40 +0200

    Adding new sulution

---
+++ Homeworks/Intro Programing/PrintFirstandLastName/printFirstandLastName.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class printFirstandLastName
+{
+    static void Main()
+    {
+        Console.WriteLine("Vladi");
+        Console.WriteLine("Vladev");
+    }
+}
---
+++ Homeworks/Intro Programing/PrintSequence/printSequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class PrintSequence
+{
+    static void Main()
+    {
+        for (int number = 2; number < 12; number++)
+        {
+            if (number % 2 == 0)
+            {
+                Console.Write(number + ", ");
+            }
+            else
+                Console.Write(number * (-1) + ", ");
+        }
+        Console.WriteLine("\n");
+    }
+}
