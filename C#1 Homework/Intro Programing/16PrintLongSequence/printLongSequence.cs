5af65020e26292ce30a31c7c6132dda0286ee05a
Vladeff <vladivivaldi@abv.bg>
2015-01-17 20:25:54 +0200

    latest

---
+++ C#1 Homework/Intro Programing/16PrintLongSequence/printLongSequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class printLongSequence
+{
+    static void Main()
+    {
+        for (int number = 2; number < 1001; number++)
+        {
+            if (number % 2 == 0)
+            {
+                Console.Write(number + ", ");
+            }
+            else
+                Console.Write(number * (-1) + ", ");
+        }
+        Console.WriteLine("\n");
+    }
+}
