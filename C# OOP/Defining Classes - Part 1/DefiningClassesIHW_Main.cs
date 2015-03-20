de6993be5862e1b825ab6cd8a2032da13571e7fd
Vladeff <vladivivaldi@abv.bg>
2015-03-20 23:01:13 +0200

    Added first homework

---
+++ C# OOP/Defining Classes - Part 1/DefiningClassesIHW_Main.cs
@@ -0,0 +1,28 @@
+﻿//One project with several classes for all of the problems
+
+using System;
+
+namespace DefiningClassesPartI
+{
+    public class Person
+    {
+        public Person(string firstName)
+        {
+            this.Name = firstName;
+        }
+        public string Name { get; private set; }
+
+        public void callName()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+    public class DefiningClassesIHW_Main
+    {
+        static void Main()
+        {
+            var pesho = new Person("Pesho");
+            pesho.callName();
+        }
+    }
+}
---
+++ Telerik/Arrays/01allocateArray/allocateArray.cs
@@ -0,0 +1,27 @@
+﻿/*Problem 1. Allocate array
+
+Write a program that allocates array of 20 integers and initializes 
+each element by its index multiplied by 5.
+Print the obtained array on the console.*/
+
+using System;
+using System.Collections.Concurrent;
+    class allocateArray
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please, enter 20 integers to multiply by 5:");
+            int[] numberArray = new int[20];
+            
+            for (int i = 0; i < 20; i++)
+            {
+                numberArray[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                int newValue = numberArray[i] * 5;
+                Console.WriteLine(newValue);
+            }
+        }
+    }
+
